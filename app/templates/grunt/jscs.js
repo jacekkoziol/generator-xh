c751c5a249f1337ae4a9342f6d0729c475cc8565
Jacek Koziol <jacek.koziol@xfive.co>
2016-02-18 13:17:32 +0100

    Changes in jsrc config file

--- app/templates/grunt/jscs.js
+++ app/templates/grunt/jscs.js
@@ -7,39 +7,7 @@ module.exports = function(grunt) {
   grunt.config('jscs', {
     src: '<%%= xh.dist %>/js/main.js',
     options : {
-      preset: "google",
-      fileExtensions: [".js", "jscs"],
-
-      requireSemicolons: true,
-      requireParenthesesAroundIIFE: true,
-      maximumLineLength: 120,
-      validateLineBreaks: "LF",
-      validateIndentation: 2,
-      disallowTrailingComma: true,
-      disallowUnusedParams: true,
-
-      disallowSpacesInsideObjectBrackets: null,
-      disallowImplicitTypeConversion: ["string"],
-
-      safeContextKeyword: "_this",
-
-      jsDoc: {
-          checkAnnotations: "closurecompiler",
-          checkParamNames: true,
-          requireParamTypes: true,
-          checkRedundantParams: true,
-          checkReturnTypes: true,
-          checkRedundantReturns: true,
-          requireReturnTypes: true,
-          checkTypes: "capitalizedNativeCase",
-          checkRedundantAccess: true,
-          requireNewlineAfterDescription: true
-      },
-
-      excludeFiles: [
-          "test/data/**",
-          "patterns/*"
-      ]
+      config: '.jscsrc'
     }
   });
 };
